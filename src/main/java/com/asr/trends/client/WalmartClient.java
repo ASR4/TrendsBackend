208455e5563bbc39699b41026e56d4bab3cac9f7
Maitri <mchattopadhyay@ebay.com>
2018-08-18 23:18:07 -0700

    ebay, walmart client

--- src/main/java/com/asr/trends/client/WalmartClient.java
+++ src/main/java/com/asr/trends/client/WalmartClient.java
@@ -1,12 +1,18 @@
 package com.asr.trends.client;
 
+import com.asr.trends.helper.UTF8ToRawUrl;
 import com.asr.trends.model.Trend;
 import com.asr.trends.model.Trends;
-import com.asr.trends.model.stocktwits.StockTwits;
-import com.asr.trends.model.stocktwits.Symbols;
+import com.asr.trends.model.walmart.Items;
+import com.asr.trends.model.walmart.Walmart;
 import com.google.appengine.repackaged.org.codehaus.jackson.JsonParseException;
+import com.google.appengine.repackaged.org.codehaus.jackson.map.DeserializationConfig;
 import com.google.appengine.repackaged.org.codehaus.jackson.map.JsonMappingException;
 import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,24 +24,25 @@ import java.util.Properties;
 
 public class WalmartClient {
 
-    private static String API_KEY;
+    private static String API_KEY_WALMART;
 
+    //constructor
     public WalmartClient(){
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream input = classLoader.getResourceAsStream("config.properties");
         Properties properties = new Properties();
         try {
             properties.load(input);
-            API_KEY = properties.getProperty("Wallmart_api_key");
+            API_KEY_WALMART = properties.getProperty("wallmart_api_key");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    public Trends getTrendingSymbols() {
+    public Trends getTrendingItems() {
 
-        String url = "http://api.walmartlabs.com/v1/trends?apiKey=" + API_KEY;
+        String url = "http://api.walmartlabs.com/v1/trends?apiKey=" + API_KEY_WALMART;
         Trends trends = new Trends();
 
         try{
@@ -50,28 +57,34 @@ public class WalmartClient {
             StringBuffer result = new StringBuffer();
             String line = "";
             while ((line = rd.readLine()) != null) {
-//				System.out.println(line);
+				System.out.println(line);
                 result.append(line);
             }
 
             //Converting json to Stocktwits pojo and then to Trends pojo
             System.out.println(jsonToPojo(result.toString()));
-            StockTwits stockTwits = jsonToPojo(result.toString());
-            List<Symbols> symbols = stockTwits.getSymbols();
+            Walmart walmart = jsonToPojo(result.toString());
+            List<Items> items = walmart.getItems();
 
             List<Trend> listOfTrend = new ArrayList<Trend>();
-            for(Symbols symbol : symbols) {
+            for(Items item  : items) {
                 Trend trend = new Trend();
-                trend.setTitle(symbol.getTitle());
-                trend.setLink("https://stocktwits.com/symbol/" + symbol.getSymbol());
-                trend.setImage(symbol.getSymbol());
+                trend.setTitle(item.getName());
+                UTF8ToRawUrl.setUrl(item.getProductUrl());
+                trend.setLink( UTF8ToRawUrl.getResult());
+                trend.setImage(item.getThumbnailImage());
                 listOfTrend.add(trend);
             }
 
-            trends.setLogo("replace_logo_from_resource");
+
+            //https://www.walmart.com/ip/Sandisk-128-GB-Ultra-Microsdxc-Memory-Card-with-Adapter/46700585?sourceid=api014811cc63ce3142b4961218af0c0c31e4&affp1=DxetGLZg9faaknZvevdd5-n_TulIRdv5HmHFjm8l078&affilsrc=api&veh=aff&wmlspartner=readonlyapi
+            //https://www.walmart.com/ip/Google-Play-100-Gift-Code-Email-Delivery/52897898?sourceid=api0129e446558191478497842d4b182e8adf&affp1=DxetGLZg9faaknZvevdd5-n_TulIRdv5HmHFjm8l078&affilsrc=api&veh=aff&wmlspartner=readonlyapi
+            // trends.setLogo("replace_logo_from_resource");
+
+
             trends.setNumOfTrends("10");
             trends.setTrend(listOfTrend.subList(0, 9));
-            trends.setType("StockTwits");
+            trends.setType("Walmart");
 
             return trends;
         }
@@ -82,12 +95,13 @@ public class WalmartClient {
         return trends;
     }
 
-    private StockTwits jsonToPojo(String json) {
+    private Walmart jsonToPojo(String json) {
         ObjectMapper mapper = new ObjectMapper();
 
-        StockTwits stockTwits = null;
+        Walmart walmart = null;
         try {
-            stockTwits = mapper.readValue(json, StockTwits.class);
+            mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            walmart = mapper.readValue(json, Walmart.class); // parent class
         } catch (JsonParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -98,14 +112,14 @@ public class WalmartClient {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println(stockTwits);
+        System.out.println(walmart);
 
-        return stockTwits;
+        return walmart;
     }
 
     public static void main(String args[]) {
-        StockTwitsClient stockTwitsClient = new StockTwitsClient();
-        stockTwitsClient.getTrendingSymbols();
+        WalmartClient walmart = new WalmartClient();
+        walmart.getTrendingItems();
     }
 }
 
